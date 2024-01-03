796f6ec5685917824cc834c50217aa67f8454ce5
vladimirruppel <minecraft20044@mail.ru>
2024-01-03 17:18:49 +0700

    Перенос инициализации исходных значений для последующих алгоритмов; написание метода displayLists

--- Main.java
+++ Main.java
@@ -2,37 +2,34 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<TrackList> lists = new ArrayList<>();
+
+        AudioFile track1 = new AudioFile("file1.mp3", "Kanye West", "Good Morning", 203, 2007);
+        AudioFile track2 = new AudioFile("file2.mp3", "Kanye West", "Ni**as in Paris", 243, 2011);
+        AudioFile track3 = new AudioFile("file3.mp3", "Скриптонит", "Космос", 181, 2017);
+        AudioFile track4 = new AudioFile("file4.mp3", "FRIENDLY THUG 52 NGG", "No Gletcher Gang", 173, 2023);
+
+        ArrayList<AudioFile> albumTracks = new ArrayList<AudioFile>();
+        albumTracks.add(track1); albumTracks.add(track2);
+
         try {
-            Album album = new Album("Kanye West", "Graduation", new ArrayList<>());
+            lists.add(new Playlist("Крутой плейлист", track1, track2, track3));
+            lists.add(new TrackList(track4));
+            lists.add(new Album("Kanye West", "Graduation", albumTracks)); // конструктор выбрасывает исключения
         }
         catch (Exception e) {
             System.out.println(e.toString());
         }
 
-        AudioFile audioFile = new AudioFile("file1.mp3", "Kanye West", "Good Morning", 240, 2007);
-        ArrayList<AudioFile> arrayList = new ArrayList<AudioFile>();
-        arrayList.add(audioFile);
-        Album album = new Album("Kanye West", "Graduation", arrayList);
+        System.out.println("В самом начале:");
+        DisplayLists(lists);
+    }
 
-        MusicLibrary musicLibrary = new MusicLibrary(audioFile);
-
-        System.out.println("Использование шаблона класса: ");
-        DisplayParent<Album> albumDisplayParent = new DisplayParent<>();
-        albumDisplayParent.Display(album);
-        DisplayParent<MusicLibrary> musicLibraryDisplayParent = new DisplayParent<>();
-        musicLibraryDisplayParent.Display(musicLibrary);
-
-        try {
-            System.out.println("Мелкое и глубокое копирование: ");
-            AudioFile audioFileClone = audioFile.clone();
-            audioFileClone.PrintInfoShort();
-
-            Album albumClone = album.clone();
-            albumClone.DisplayShort();
+    private static void DisplayLists(ArrayList<TrackList> lists) {
+        int i = 1;
+        for (TrackList list : lists) {
+            System.out.print("Треклист #" + i++ + ":\n");
+            list.Display();
         }
-        catch (CloneNotSupportedException e) {
-            System.out.println(e.toString());
-        }
-
     }
 }
